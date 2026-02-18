1a66cd1af103142d58fcd5ea268a00ee1abf3f66
shanib-techie <shanibkhan622@gmail.com>
2026-02-18 12:59:14 +0530

    file

--- histograph.py
+++ histograph.py
@@ -2,15 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 df = pd.read_excel("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\retail_store_analysis\\Retail-Store-Transactions (1).xlsx")
-print(df.describe())
+print("overall : ",df.describe())
 print(df)
-print(df["Product"].unique())
+print("unique va",df["Product"].unique())
 k = df["Product"]
+plt.xlabel("products ")
+plt.ylabel("count of eeach product")
+print("count of product")
 plt.hist(k,bins=4,color="pink",edgecolor="black") # iss row meyeh perticular work kitni baar aya h graph me ajata h
 
 
 
-# print(df[Product])
+# print(df["Product"])
 # score = np.array([23,44,2,1,33,44,55,6,44,3,76,88,8,77,5,44,3,5,43,2,44,6,21,23,23,4,5,77,65,43,3,2,22,11,77,6,55,4,33,7,77,52,22,46])
 # print(score)
 
--- pie_chart.py
+++ pie_chart.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # plt.pie(vluee,labels=label_list,color=color_list,autopct="%1.1f")
 
-# data = pd.read_csv("")
+data = pd.read_csv("C:\\Users\\Admin\\OneDrive\\Desktop\\power bi\\retail_store_analysis\\Retail-Store-Transactions (1).xlsx")
 emplname = ["shanib","ahmed","ujju","bobby","kanay"]
 incomee =[10000,300000,34000,10000,30000]
 plt.pie(incomee,labels=emplname,autopct="%1.1f%%",colors=["pink","green","red","yellow","blue"])
