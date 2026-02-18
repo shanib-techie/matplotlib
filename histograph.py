d036e931f29754a6e40bee29bc839752c0078fd5
shanib-techie <shanibkhan622@gmail.com>
2026-02-18 13:37:50 +0530

    ad

--- histograph.py
+++ histograph.py
@@ -9,9 +9,9 @@ k = df["Product"]
 plt.xlabel("products ")
 plt.ylabel("count of eeach product")
 print("count of product")
-plt.hist(k,bins=4,color="pink",edgecolor="black") # iss row meyeh perticular work kitni baar aya h graph me ajata h
+plt.hist(k,bins=6,color="pink",edgecolor="black") # iss row meyeh perticular work kitni baar aya h graph me ajata h
 
-
+print(df["Product"].value_counts())
 
 # print(df["Product"])
 # score = np.array([23,44,2,1,33,44,55,6,44,3,76,88,8,77,5,44,3,5,43,2,44,6,21,23,23,4,5,77,65,43,3,2,22,11,77,6,55,4,33,7,77,52,22,46])
--- scatter_graph.py
+++ scatter_graph.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as plt
+# 
+# otlib.pyplot as plt
+import matplotlib as plt
 # hour_studies = [1,2,3,4,5,6,7,8]
 # exam_score = [50,54,33,15,88,66,55,45]
 # plt.scatter(hour_studies,exam_score,color="green",marker="^",label = "student data")#mark="o","s","^","+","d"
